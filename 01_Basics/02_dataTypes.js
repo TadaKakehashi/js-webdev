75c65f55b284f8eafb69eeac42dad73598123aba
Aryan Mohanty <aryan333457@gmail.com>
2024-09-16 02:04:17 +0000

    Data_Types

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,17 @@
+"use strick" //treat all JS code as newer version
+
+let name = "aryan";
+// alert("Hello") we are using NodeJs and Not browser engine
+
+//primitive 
+//number => 2 to power 53
+//bigint
+//string = "", ''
+//boolean => true/false
+//null = standalone value, representation of an empty value // its an object (check with typeof)
+//undefined = when value is not defined 
+//symbol = to get uniqueness for an individual component 
+
+//object
+
+console.log(typeof undefined)
