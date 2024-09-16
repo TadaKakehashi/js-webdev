c7e2b9f557f032a1a1a38e634ff92f650f64b88e
Aryan Mohanty <aryan333457@gmail.com>
2024-09-16 06:29:29 +0000

    TypeCasting

---
+++ 01_Basics/03_convoOps.js
@@ -0,0 +1,14 @@
+// // let score = "10";
+// console.log(typeof (score));
+
+// let booleanIndeed = Boolean(score);
+// //purenumber has to be used otherwise it gives a NaN (Not a Number)
+// console.log(booleanIndeed);
+
+let string = "Tada";
+let num = Number(string);
+console.log(num);
+
+let string1 = "check";
+let booleanCheck = Boolean(string1);
+console.log(booleanCheck);
