ac8d023fed5672db6f38b1b2213639a221843a4e
Aryan Mohanty <aryan333457@gmail.com>
2024-09-16 01:31:30 +0000

    check

---
+++ 01_Basics/01_var.js
@@ -0,0 +1,7 @@
+const accountID = 10;
+let accountName = "Tada";
+var accountEmail = "Tada@gmail.com";
+accountState = "Hold";
+let accountBalance;
+
+console.table([accountID, accountName, accountEmail, accountState, accountBalance]);
