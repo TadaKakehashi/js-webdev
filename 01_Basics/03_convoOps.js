6f70739bb50895f88585898e99f17f3bfbc6c653
Aryan Mohanty <aryan333457@gmail.com>
2024-09-17 05:14:36 +0000

    Operations

--- 01_Basics/03_convoOps.js
+++ 01_Basics/03_convoOps.js
@@ -5,10 +5,21 @@
 // //purenumber has to be used otherwise it gives a NaN (Not a Number)
 // console.log(booleanIndeed);
 
-let string = "Tada";
-let num = Number(string);
-console.log(num);
+// let string = "Tada";
+// let num = Number(string);
+// console.log(num);
 
-let string1 = "check";
-let booleanCheck = Boolean(string1);
-console.log(booleanCheck);
+// let string1 = "check";
+// let booleanCheck = Boolean(string1);
+// console.log(booleanCheck);
+
+/****************Operations*************************/
+
+let num = 10
+let negNum = -num
+console.log(negNum);
+
+let str1 = "tada"
+let str2 = " kakehashi"
+console.log(str1 + str2);
+
